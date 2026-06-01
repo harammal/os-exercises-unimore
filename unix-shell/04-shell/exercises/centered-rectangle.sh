a8a77071b46727fe7c835ae34ed9d7a9270185c0
harammal <hassanrammal005@proton.me>
2026-06-01 13:19:17 +0200

    Add centered-rectangle.sh

---
+++ unix-shell/04-shell/exercises/centered-rectangle.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+draw_rect() {
+	# read the variables
+	local x1=$1
+	local y1=$2
+	local x2=$3
+	local y2=$4
+	# calculate length and height
+	local l=$((x2 - x1))
+	local h=$((y2 - y1))
+
+	# coordinates and data for debug
+	#echo "x1=$x1, y1=$y1, x2=$x2, y2=$y2"
+	#echo "l=$l, h=$h"
+	#echo "cols=$(tput cols), lines=$(tput lines)"
+
+	# printf upper base
+	tput cup $y1 $x1
+	for (( i=0; i<l; i++ )); do
+		printf "+"
+	done
+	echo ""
+
+	#print both heights with a custom background color
+	for (( i=0; i<h-2; i++ )); do
+		tput cuf $x1
+		printf "+\e[46m%*s\e[0m+\n" $((l-2))
+	done
+
+	# print lower  base
+	tput cup $((y2-1)) $x1
+	for (( i=0; i<l; i++ )); do
+		printf "+"
+	done
+	echo ""
+}
+
+rect() {
+	# calculate length and height
+	local l=$(echo "$(tput cols) / 2" | bc)
+	local h=$(echo "$(tput lines) / 2" | bc)
+
+	# calculate the coordinates
+	local x1=$((l / 2))
+	local y1=$((h / 2))
+	local x2=$((l + l / 2))
+	local y2=$((h + h / 2))
+
+	# call the function passing the coordinates
+	draw_rect $x1 $y1 $x2 $y2
+}
+
+# SIGWINCH handler
+trap 'clear; rect' SIGWINCH
+
+# SIGINT handler
+trap 'clear; exit' SIGINT
+
+# start
+clear
+rect
+
+# main loop
+while true; do
+	sleep 1
+done
